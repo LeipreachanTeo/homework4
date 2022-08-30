7a467d17fc2e218fe169003c790e8d3bde7ec345
LeipreachanTeo <pokiboom@mail.ru>
2022-08-30 17:52:05 +0700

    Second task

---
+++ second_task/second_task.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int copy_number = number;
+int result = 0;
+try
+{
+   while(copy_number > 0)
+   {
+      int last_digit = copy_number % 10;
+      if(number % last_digit == 0)
+      {
+         result += last_digit;
+      } 
+      copy_number /= 10;
+   }
+}
+catch
+{}
+Console.WriteLine(result);
