cfa400bf2f84342bf9de9768b611d7a63e3d3ced
LeipreachanTeo <pokiboom@mail.ru>
2022-08-30 17:20:44 +0700

    First_task

---
+++ first_task/first_task.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число, которое нужно возвести в степень: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число степени, в которую нужно возвести: ");
+int B = int.Parse(Console.ReadLine());
+int i;
+int result = 1;
+for (i = 0; i < B; i++)
+{
+    result *= A;
+}
+Console.WriteLine(result);
