c404d2f647335a15e5e10e0785fe05ce9a144db0
LeipreachanTeo <pokiboom@mail.ru>
2022-08-31 08:11:59 +0700

    update second task

--- second_task/second_task.cs
+++ second_task/second_task.cs
@@ -2,18 +2,15 @@
 int number = int.Parse(Console.ReadLine());
 int copy_number = number;
 int result = 0;
-try
+
+while(copy_number > 0)
 {
-   while(copy_number > 0)
+   int last_digit = copy_number % 10;
+   if(last_digit != 0 && number % last_digit == 0)
    {
-      int last_digit = copy_number % 10;
-      if(number % last_digit == 0)
-      {
-         result += last_digit;
-      } 
-      copy_number /= 10;
-   }
+      result += last_digit;
+   } 
+   copy_number /= 10;
 }
-catch
-{}
+
 Console.WriteLine(result);
